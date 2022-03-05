169fb73a660c28a0281d9a6a4f08f7c6e22cf642
joshmeranda <joshmeranda@gmail.com>
2022-03-05 12:54:42 -0500

    implement pushing

--- git.py
+++ git.py
@@ -5,7 +5,24 @@ from os import path
 import subprocess
 
 import pygit2
-from pygit2 import GitError, RemoteCallbacks, Repository, Signature
+from pygit2 import credentials, GitError, Keypair, RemoteCallbacks, Repository, Signature, Username
+
+
+class SshRemoteCallbacks(RemoteCallbacks):
+    def __init__(self, private: str, public: str):
+        """Provides the necessary for communicating with GitHub over ssh."""
+        super().__init__()
+
+        self.__private = private
+        self.__public = public
+
+    def credentials(self, url, username_from_url, allowed_types):
+        if allowed_types & credentials.GIT_CREDENTIAL_USERNAME:
+            return Username("git")
+        elif allowed_types & credentials.GIT_CREDENTIAL_SSH_KEY:
+            return Keypair("git", self.__public, self.__private, "")
+        else:
+            return None
 
 
 class Driver:
@@ -16,7 +33,8 @@ class Driver:
     def __init__(self, repo_path: str,
                  https: Optional[str] = None,
                  ssh: Optional[str] = None,
-                 clone_callbacks: Optional[RemoteCallbacks] = None):
+                 clone_callbacks: Optional[RemoteCallbacks] = None,
+                 push_callbacks: Optional[RemoteCallbacks] = None):
         """Driver is a wrapper around all git operations.
 
         If neither `https` not `ssh` are specified, `Driver` will attempt to initialize a git repository at that path.
@@ -31,6 +49,7 @@ class Driver:
 
         https://www.pygit2.org/recipes.html#main-porcelain-commands
         """
+        self.__push_callbacks = push_callbacks
 
         if ssh is not None:
             self.__repo: Repository = pygit2.clone_repository(ssh, repo_path, callbacks=clone_callbacks)
@@ -84,3 +103,16 @@ class Driver:
                 _proc = subprocess.run(f"git commit --amend --no-edit --date={new_date}".split(),
                                        cwd=path.dirname(self.__repo.path.rstrip(path.sep)),
                                        capture_output=True)
+
+    def push(self, remote_name: str = "origin"):
+        """Push to the remote with the given name.
+
+        :param remote_name: The name of the target origin.
+        """
+        try:
+            remote = self.__repo.remotes[remote_name]
+        except KeyError:
+            return ValueError(f"no such remote '{remote_name}' exists for the given repo")
+
+        # todo: we should accept this from user, or determine the default branch name
+        remote.push(["refs/heads/main"], callbacks=self.__push_callbacks)
